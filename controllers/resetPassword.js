d52d13b7a9f5eb083b30b065bef1eb867d48ccc9
tolaji <39959545+Tolaji@users.noreply.github.com>
2025-06-06 16:31:40 +0100

    minor

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    service: 'Gmail', // you can configure other services or SMTP here
+    service: 'Gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
